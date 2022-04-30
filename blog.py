d0a6850a8bc21e1f7dd7460d620e922634a48b7e
Eduardo Lopez <eduardo@myportfolio.tech>
2022-04-30 13:50:59 -0400

    Added the field errors display on register.html

--- blog.py
+++ blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -34,6 +34,9 @@ def about():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
--- forms.py
+++ forms.py
@@ -7,10 +7,11 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=5, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[
+                        DataRequired(), Email('Enter A Valid Email')])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField(
-        'Confirm Password', validators=[DataRequired(), EqualTo('password')])
+        'Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
 
     submit = SubmitField("Sign Up")
 
