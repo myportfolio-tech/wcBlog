181a5cc4b3f4f14f87ee422cb82dc40c6a2beed1
Eduardo Lopez <eduardo@myportfolio.tech>
2022-04-30 14:20:46 -0400

    Added messages of failure & success at login

--- blog.py
+++ blog.py
@@ -40,9 +40,15 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in successfully', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Loging failed. Please check email and password', 'danger')
     return render_template('login.html', title='Sign In', form=form)
 
 
