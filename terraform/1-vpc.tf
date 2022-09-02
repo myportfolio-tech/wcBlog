1316739611437edbd8b6cc0c70ee391a88a4ab87
Eduardo Lopez <eduardo@myportfolio.tech>
2022-09-02 16:07:51 -0400

    terraform 2-igw file - small edit on 1-vpc

--- terraform/1-vpc.tf
+++ terraform/1-vpc.tf
@@ -2,6 +2,6 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "main"
+    Name = "weknowx"
   }
 }
---
+++ terraform/2-igw.tf
@@ -0,0 +1,11 @@
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.weknowx.id
+
+  tags = {
+    Name = "igw-weknowX"
+  }
+}
+
+
+
