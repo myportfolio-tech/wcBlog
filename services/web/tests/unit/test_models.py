85cdeb5c4695b072f1d314545178827e186c24fc
Eduardo Lopez <eduardo@myportfolio.tech>
2022-09-12 14:54:32 -0400

    Created the test folders/ test_models.py

---
+++ services/web/tests/unit/test_models.py
@@ -0,0 +1,6 @@
+from weblog.models import User
+
+def test_new_user():
+
+    user = User(username='username01', email='username01@email.com', image_file='default.jpg', password='test-password')
+    assert user.email == 'username01@email.com'
