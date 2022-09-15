8fb5a20730ac8dfd877b8a654f623e398046d113
Eduardo Lopez <eduardo@myportfolio.tech>
2022-09-15 16:05:58 -0400

    set pytest to fail to test github action flow

--- services/web/tests/unit/test_models.py
+++ services/web/tests/unit/test_models.py
@@ -3,4 +3,4 @@ from weblog.models import User
 def test_new_user():
 
     user = User(username='username01', email='username01@email.com', image_file='default.jpg', password='test-password')
-    assert user.email == 'username01@email.com'
+    assert user.email == 'username012@email.com'
