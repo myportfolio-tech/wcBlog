7cab43970dfebf9a18132237e326319467890e54
Eduardo Lopez <eduardo@myportfolio.tech>
2022-09-19 14:20:29 -0400

    fixed networks in 3-subnets.tf and deleted old services.yml

--- terraform/3-subnets.tf
+++ terraform/3-subnets.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "private-us-east-1a" {
+resource "aws_subnet" "dev-private-us-east-1a" {
   vpc_id            = aws_vpc.dev-weknowx.id
   cidr_block        = "10.0.0.0/19"
   availability_zone = "us-east-1a"
@@ -11,7 +11,7 @@ resource "aws_subnet" "private-us-east-1a" {
   }
 }
 
-resource "aws_subnet" "private-us-east-1b" {
+resource "aws_subnet" "dev-private-us-east-1b" {
   vpc_id            = aws_vpc.dev-weknowx.id
   cidr_block        = "10.0.32.0/19"
   availability_zone = "us-east-1b"
@@ -24,7 +24,7 @@ resource "aws_subnet" "private-us-east-1b" {
   }
 }
 
-resource "aws_subnet" "public-us-east-1a" {
+resource "aws_subnet" "dev-public-us-east-1a" {
   vpc_id                  = aws_vpc.dev-weknowx.id
   cidr_block              = "10.0.64.0/19"
   availability_zone       = "us-east-1a"
@@ -38,7 +38,7 @@ resource "aws_subnet" "public-us-east-1a" {
   }
 }
 
-resource "aws_subnet" "public-us-east-1b" {
+resource "aws_subnet" "dev-public-us-east-1b" {
   vpc_id                  = aws_vpc.dev-weknowx.id
   cidr_block              = "10.0.96.0/19"
   availability_zone       = "us-east-1b"
